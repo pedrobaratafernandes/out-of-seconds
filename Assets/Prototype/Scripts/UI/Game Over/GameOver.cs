46324e0318eae913cf8c59584ab51717d4b58d71
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-04-06 14:06:17 +0100

    fix: resolve double enter issue in game over screen to main menu

--- Assets/Prototype/Scripts/UI/Game Over/GameOver.cs
+++ Assets/Prototype/Scripts/UI/Game Over/GameOver.cs
@@ -3,8 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
+    private bool isExiting = false;
     public void GoToMainMenu()
     {
+        if (isExiting)
+        {
+            return;
+        }
+        isExiting = true;
         SceneManager.LoadScene("Main Menu");
     }
 }
